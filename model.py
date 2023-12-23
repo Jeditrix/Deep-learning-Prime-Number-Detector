fb2b50816b778e022263fe47cbd7bf0fae476be3
Jeditrix <107357374+Jeditrix@users.noreply.github.com>
2023-12-23 01:39:16 -0500

    Update model.py

--- model.py
+++ model.py
@@ -7,11 +7,11 @@ import torch
 #isprime function, could possibly be replaced if you wanted to train a larger range of numbers 
 def is_prime(n):
     if n <= 1:
-        False
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            False
-    True
+            return False
+    return True
 #here are a few functions for features to train the model on, feel free to try and incorperate more and test    
 def sum_of_digits(n):
     return sum(int(digit) for digit in str(n))
@@ -25,7 +25,17 @@ def num_divisors(n):
 
 def last_digit(n): return n % 10
 
-def modulo_6(n): return n % 6  
+def modulo_6(n): return n % 6
+
+def prime_gap(n):
+    def next_prime(num):
+        def is_prime(k):
+            return all(k % i for i in range(2, int(k**0.5) + 1)) and k > 1
+        prime_candidate = num + 1
+        while not is_prime(prime_candidate):
+            prime_candidate += 1
+        return prime_candidate
+    return next_prime(n) - n
 
 
 def custom_transform(x):
@@ -34,12 +44,13 @@ def custom_transform(x):
         'num_divisors': num_divisors(x),
         'last_digit': last_digit(x),
         'modulo_6': modulo_6(x),
-        'is_prime': is_prime(x)
+        'is_prime': is_prime(x),
+        'prime_gap': prime_gap(x)
     })
     
 def load_data():
     # right here is the number range to train on
-    numbers = np.arange(1, 90000)
+    numbers = np.arange(1, 1000000)
     data = pd.DataFrame({"number": numbers})
     data_transformed = data['number'].apply(custom_transform)
     data = pd.concat([data, data_transformed], axis=1)
@@ -47,9 +58,9 @@ def load_data():
 
 def train_model(train_data):
     dls = TabularDataLoaders.from_df(train_data, path='.', y_names="is_prime",
-                                     cat_names=None, cont_names=['sum_of_digits', 'num_divisors', 'last_digit', 'modulo_6'],
-                                     procs=[Categorify, FillMissing, Normalize],
-                                     valid_pct=0.2, seed=42, bs=64)
+                                 cat_names=None, cont_names=['sum_of_digits', 'num_divisors', 'last_digit', 'modulo_6', 'prime_gap'],
+                                 procs=[Categorify, FillMissing, Normalize],
+                                 valid_pct=0.2, seed=42, bs=64)
     learn = tabular_learner(dls, layers=[3000,2000,1000], metrics=accuracy)
     learn.fit_one_cycle(10)
     return learn
@@ -57,7 +68,7 @@ def train_model(train_data):
 def main():
     data = load_data()
     learn = train_model(data)
-    learn.export('primenum.pkl')
+    learn.export('C:/Users/alexh/Downloads/primenum.pkl')
 
 if __name__ == "__main__":
     main()
