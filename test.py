10900ea328372433d2d28c5a9b37c3a1df3034d1
Jeditrix <107357374+Jeditrix@users.noreply.github.com>
2023-12-23 01:39:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,51 @@ import pandas as pd
 import numpy as np
 import torch
 
+
+#isprime function, could possibly be replaced if you wanted to train a larger range of numbers 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+#here are a few functions for features to train the model on, feel free to try and incorperate more and test    
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+def num_divisors(n):
+    divisors = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            divisors += 2
+    return divisors - 1 if n == int(n**0.5)**2 else divisors
+
+def last_digit(n): return n % 10
+
+def modulo_6(n): return n % 6
+
+def prime_gap(n):
+    def next_prime(num):
+        def is_prime(k):
+            return all(k % i for i in range(2, int(k**0.5) + 1)) and k > 1
+        prime_candidate = num + 1
+        while not is_prime(prime_candidate):
+            prime_candidate += 1
+        return prime_candidate
+    return next_prime(n) - n
+
+def custom_transform(x):
+    return pd.Series({
+        'sum_of_digits': sum_of_digits(x),
+        'num_divisors': num_divisors(x),
+        'last_digit': last_digit(x),
+        'modulo_6': modulo_6(x),
+        'is_prime': is_prime(x),
+        'prime_gap': prime_gap(x)
+    })
 def load_test_data():
-    test_numbers = np.arange(90001,100000)
+    test_numbers = np.arange(1000001,1100001)
     test_data = pd.DataFrame({"number": test_numbers})
     test_transformed = test_data['number'].apply(custom_transform)
     test_data = pd.concat([test_data, test_transformed], axis=1)
